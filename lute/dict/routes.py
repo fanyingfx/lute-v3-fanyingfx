25f8a18b07367779309f088848caa49e29fbc2e9
fanyingfx <fanyingfx@outlook.com>
2023-12-08 21:48:15 +0800

    add more dicts

--- lute/dict/routes.py
+++ lute/dict/routes.py
@@ -22,17 +22,22 @@ filedir = "/home/fan/dicts/Eng/olad10"
 en_filename = "Oxford Advanced Learner's Dictionary 10th.mdx"
 jp_filedir = "/home/fan/dicts/ja/Shogakukanjcv3"
 jp_filename = "Shogakukanjcv3.mdx"
+jp_filename2 = "xsjrihanshuangjie.mdx"
+jp_dir2 = "/home/fan/dicts/ja/xsjrihanshuangjie"
 resource_path = Path(filedir)
 jp_resource_path = Path(jp_filedir)
+jp_res2 = Path(jp_dir2)
 
-en_local = get_local_resource([resource_path, jp_resource_path])
-jp_local = get_local_resource([jp_resource_path])
+dict_local = get_local_resource([resource_path, jp_resource_path, jp_res2])
+# jp_local = get_local_resource([jp_resource_path])
 
 builder = IndexBuilder(f"{filedir}/{en_filename}")
 jp_builder = IndexBuilder(f"{jp_filedir}/{jp_filename}")
+jp_builder2 = IndexBuilder(f"{jp_dir2}/{jp_filename2}")
 
-bd = MDXDict(builder, en_local)
-jp_bd = MDXDict(jp_builder, jp_local)
+bd = MDXDict(builder, dict_local)
+jp_bd = MDXDict(jp_builder, dict_local)
+jp_bd2 = MDXDict(jp_builder2, dict_local)
 
 
 def handle_req(filename, loc):
@@ -54,14 +59,14 @@ def handle_req(filename, loc):
 @bp.route("/<path:path>")
 def index(path):
     print("path", path)
-    return handle_req(path, en_local)
+    return handle_req(path, dict_local)
 
 
 @bp.route("/en/<word>")
 def query_en(word):
     ext = word.split(".")[-1]
     if ext in content_type_map:
-        return handle_req(word, en_local)
+        return handle_req(word, dict_local)
     res = bd.lookup(word)
     response = make_response(res)
     content_type = content_type_map["html"]
@@ -74,8 +79,11 @@ def query_jp():
     word = request.args.get("word", "")
     ext = word.split(".")[-1]
     if ext in content_type_map:
-        return handle_req(word, jp_local)
-    res = jp_bd.lookup(word)
+        return handle_req(word, dict_local)
+    for jbd in [jp_bd, jp_bd2]:
+        res = jp_bd.lookup(word)
+        if res != b"":
+            break
     response = make_response(res)
     content_type = content_type_map["html"]
     response.headers["Content-type"] = content_type
